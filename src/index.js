6ec39d9da870f59c0a00c5d9350abfcc82322b2d
Tahir Jemal <jemaltahir83@gmail.com>
2018-07-08 23:08:34 +0300

    component GameCard created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+
+import GameCard from './components/GameCard';
+
 const nodeElement = document.getElementById('root');
 
-render(<h1>Hello World!</h1>, nodeElement);
+render(<GameCard />, nodeElement);
