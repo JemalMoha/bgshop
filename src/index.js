05be256876ed9c8256f9792f9832862c4351f3f8
Tahir Jemal <jemaltahir83@gmail.com>
2018-07-08 22:29:47 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+const nodeElement = document.getElementById('root');
+
+render(<h1>Hello World!</h1>, nodeElement);
